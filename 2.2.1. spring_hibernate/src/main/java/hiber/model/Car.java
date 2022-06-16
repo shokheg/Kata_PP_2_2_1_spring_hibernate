94a2b2c147c15f84363188ed2ec34fa0ec087884
shokheg <ioga@bk.ru>
2022-06-16 21:10:39 +0300

    Test commit. Working version.

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,51 @@
+package hiber.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+/**
+ * @author shokhalevich
+ */
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String model;
+    private int series;
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+    public Car (){
+
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "id=" + id +
+                ", model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
+}
